799e349f9917da903c27141e99a0e65e97b1d6f7
Juan Diego <juandiegomariscal@gmail.com>
2022-06-28 23:02:46 +0200

    feat: 👍 clase modelo creada

---
+++ criptocambio/models.py
@@ -0,0 +1,26 @@
+#Modelo se ocupa de manejar los datos y llamar a la API
+import requests
+
+from . import APIKEY
+
+class APIError(Exception):
+    pass
+
+class CriptoModel:
+    def __init__(self, origen, destino):
+        self.moneda_origen = origen
+        self.moneda_destino = destino
+        self.cambio = 0.0
+
+    def consultar_cambio(self):
+        headers = {'X-CoinAPI-Key' : APIKEY}
+        url = f"http://rest.coinapi.io/v1/exchangerate/{self.moneda_origen}/{self.moneda_destino}"
+        respuesta = requests.get(url, headers=headers)
+        codigo = respuesta.status_code
+
+        if codigo == 200:
+            self.cambio = respuesta.json()["rate"]
+        else:
+            raise APIError("Ha ocurrido un error {} al consultar la API").format(codigo, respuesta.reason)
+
+
